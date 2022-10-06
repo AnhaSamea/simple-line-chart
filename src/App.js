42190dc70f397b503a396998a7431ce8dd101f35
anhasamea <anhasamea707@gmail.com>
2022-10-06 18:12:06 +0600

    created simple line chart

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AssignmentMarks from './components/AssignmentMarks/AssignmentMarks';
+function App() {
+  return (
+    <div className="App">
+      <h2>Simple Line Chart</h2>
+      <AssignmentMarks></AssignmentMarks>
+    </div>
+  );
+}
+
+export default App;
